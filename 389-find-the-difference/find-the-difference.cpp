ca718599e9ae2d1b132a2f5dac3b2159a00dd862
Shubhanggit <159463995+Shubhanggit@users.noreply.github.com>
2025-02-04 11:03:55 +0530

    Time: 3 ms (15.39%) | Memory: 9.1 MB (95.21%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
-        char ans = 0;
-        for(char s1 : s) ans ^= s1;
-        for(char s2 : t) ans ^= s2;
-        return ans;
-    }
+	char findTheDifference(string s, string t) {
+		sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+		int i = 0;
+		while(i < s.size()){
+			if(s[i] != t[i]){
+				return t[i];
+			}
+			i++;
+		}
+		return t[s.size()];
+	 }
 };
