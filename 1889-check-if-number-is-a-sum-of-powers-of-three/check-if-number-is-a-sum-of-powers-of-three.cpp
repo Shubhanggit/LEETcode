6ee38b6572ece4596bc0a7fd24658229f484253f
Shubhang Pandey <159463995+Shubhanggit@users.noreply.github.com>
2025-03-04 15:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.08%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-       return n%3==2?0:(n>3?checkPowersOfThree(n/3):1);  
+        if (n<=0) return 0;
+        while(n>0){
+            auto [q, r]=div(n, 3);
+            if (r==2) return 0;
+            n=q;
+        }
+        return 1;
     }
 };
