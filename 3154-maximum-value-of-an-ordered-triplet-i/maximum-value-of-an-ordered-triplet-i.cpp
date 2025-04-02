2038cf6ffa5281dd53d33636b03bfda2537e3a13
Shubhang Pandey <159463995+Shubhanggit@users.noreply.github.com>
2025-04-02 18:16:48 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (35.84%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    long long maximumTripletValue(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>arr1(n),arr2(n);
-        arr1[0]=nums[0];
-        for(int i=1;i<n;i++){
-            arr1[i]=max(arr1[i-1],nums[i]);
+    static long maximumTripletValue(const vector<int>& a) {
+        const int n = a.size();
+        long maxt = 0;
+        long maxd = 0;
+        long maxi = 0;
+        for (int k = 0; k < n; k++) {
+            const long ak = a[k];
+            maxt = max(maxt, maxd * ak);
+            maxd = max(maxd, maxi - ak);
+            maxi = max(maxi, ak);
         }
-        arr2[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            arr2[i]=max(arr2[i+1],nums[i]);
-        }
-
-        long long ans=0;
-        for(int i=1;i<n-1;i++){
-            long long val=1LL*(arr1[i-1]-nums[i])*arr2[i+1];
-            ans=max(ans,val);
-        }
-        return ans;
+        return maxt;
     }
 };
