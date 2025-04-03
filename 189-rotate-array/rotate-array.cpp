19df27c3b0d956979fc31d37c5f5b57ef9351c0b
Shubhang Pandey <159463995+Shubhanggit@users.noreply.github.com>
2025-04-03 18:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (79.45%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,7 +4,7 @@ public:
         k = k % nums.size();
        
         reverse(nums.begin() , nums.end());
-        reverse(nums.begin() , nums.begin() + k);
+        reverse(nums.begin() , nums.begin()+k );
         reverse(nums.begin() + k , nums.end());
         
 
