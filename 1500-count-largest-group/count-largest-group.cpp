f97cda76e4d7cd352b5a9ebc833ebc3ec7afcd01
Shubhang Pandey <159463995+Shubhanggit@users.noreply.github.com>
2025-04-23 12:51:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (91.19%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        vector<int> v(100, 0);  
+        for (int i = 1; i <= n; i++) {
+            int sum = 0, temp = i;
+            while (temp) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            v[sum]++;
+        }
+
+        sort(v.begin(), v.end());
+        int ans = v[v.size() - 1];
+        int count = 0;
+
+        for (int i = v.size() - 1; i >= 0; i--) {
+            if (v[i] == ans) count++;
+            else break;
+        }
+        return count;
+    }
+};
