9ea608c8d342df34a95dcd521938a0f944a90170
Shubhang Pandey <159463995+Shubhanggit@users.noreply.github.com>
2025-06-20 00:35:28 +0530

    Time: 17 ms (95.92%) | Memory: 71 MB (79.26%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-
-	unordered_set <int> s; 
-	for(auto a:nums) 
-        s.insert(a);
- 	if(nums.size()!=s.size()) // Duplicate Elements
-	    return true; 
-	else return false;
- }
-
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if (nums[i]==nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
 };
